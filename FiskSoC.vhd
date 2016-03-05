34c7d24b9189243e19c2ddf8158a9860f7e59290
Vegar Westerlund <vegarwe@gmail.com>
2016-03-05 22:08:34 +0100

    Compiling and running

--- FiskSoC.vhd
+++ FiskSoC.vhd
@@ -1,18 +1,3 @@
--- Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
--- Your use of Altera Corporation's design tools, logic functions 
--- and other software and tools, and its AMPP partner logic 
--- functions, and any output files from any of the foregoing 
--- (including device programming or simulation files), and any 
--- associated documentation or information are expressly subject 
--- to the terms and conditions of the Altera Program License 
--- Subscription Agreement, the Altera Quartus Prime License Agreement,
--- the Altera MegaCore Function License Agreement, or other 
--- applicable license agreement, including, without limitation, 
--- that your use is for the sole purpose of programming logic 
--- devices manufactured by Altera and sold by Altera or its 
--- authorized distributors.  Please refer to the applicable 
--- agreement for further details.
-
 library ieee;
 use ieee.std_logic_1164.all;
 library altera;
@@ -23,7 +8,9 @@ entity FiskSoC is
 	(
 -- {ALTERA_IO_BEGIN} DO NOT REMOVE THIS LINE!
 
-		CLOCK_50 : in std_logic
+		CLOCK_50 : in std_logic;
+		KEY : in std_logic;
+		LEDR : out std_logic_vector(0 to 7)
 -- {ALTERA_IO_END} DO NOT REMOVE THIS LINE!
 
 	);
@@ -36,8 +23,28 @@ architecture ppl_type of FiskSoC is
 
 -- {ALTERA_COMPONENTS_BEGIN} DO NOT REMOVE THIS LINE!
 -- {ALTERA_COMPONENTS_END} DO NOT REMOVE THIS LINE!
+
+-- Constants
+
+-- Signals
+	signal clk		: std_logic;
+	signal reset_n		: std_logic;
+
 begin
 -- {ALTERA_INSTANTIATION_BEGIN} DO NOT REMOVE THIS LINE!
 -- {ALTERA_INSTANTIATION_END} DO NOT REMOVE THIS LINE!
 
+	c_gpio1 : entity work.fsk_gpio
+	port map (
+		reset_n		=> reset_n,
+		clk		=> clk,
+		port0		=> LEDR(0),
+		port1		=> LEDR(1),
+		port2		=> LEDR(2),
+		port3		=> LEDR(3)
+	);
+
+	clk <= CLOCK_50;
+	reset_n <= KEY;
 end;
+
