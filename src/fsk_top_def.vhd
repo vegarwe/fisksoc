d6084a2e906210ad5c122191cc3d0893ee8f6f28
Vegar Westerlund <vegarwe@gmail.com>
2016-03-05 21:15:58 +0100

    Initial check in

---
+++ src/fsk_top_def.vhd
@@ -0,0 +1,43 @@
+-- Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
+-- Your use of Altera Corporation's design tools, logic functions 
+-- and other software and tools, and its AMPP partner logic 
+-- functions, and any output files from any of the foregoing 
+-- (including device programming or simulation files), and any 
+-- associated documentation or information are expressly subject 
+-- to the terms and conditions of the Altera Program License 
+-- Subscription Agreement, the Altera Quartus Prime License Agreement,
+-- the Altera MegaCore Function License Agreement, or other 
+-- applicable license agreement, including, without limitation, 
+-- that your use is for the sole purpose of programming logic 
+-- devices manufactured by Altera and sold by Altera or its 
+-- authorized distributors.  Please refer to the applicable 
+-- agreement for further details.
+
+library ieee;
+use ieee.std_logic_1164.all;
+library altera;
+use altera.altera_syn_attributes.all;
+
+entity fsk_top_def is
+	port
+	(
+-- {ALTERA_IO_BEGIN} DO NOT REMOVE THIS LINE!
+
+		CLOCK_50 : in std_logic
+-- {ALTERA_IO_END} DO NOT REMOVE THIS LINE!
+
+	);
+
+-- {ALTERA_ATTRIBUTE_BEGIN} DO NOT REMOVE THIS LINE!
+-- {ALTERA_ATTRIBUTE_END} DO NOT REMOVE THIS LINE!
+end fsk_top_def;
+
+architecture ppl_type of fsk_top_def is
+
+-- {ALTERA_COMPONENTS_BEGIN} DO NOT REMOVE THIS LINE!
+-- {ALTERA_COMPONENTS_END} DO NOT REMOVE THIS LINE!
+begin
+-- {ALTERA_INSTANTIATION_BEGIN} DO NOT REMOVE THIS LINE!
+-- {ALTERA_INSTANTIATION_END} DO NOT REMOVE THIS LINE!
+
+end;
